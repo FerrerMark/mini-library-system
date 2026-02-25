e60f2a724ab7bfcac907bc282226150ad8b45404
FerrerMark <ferrerjohnmark26@gmail.com>
2026-02-26 00:21:32 +0800

    fix: serve React build and enable client-side routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 import { connectDB } from "./config/db.js";
 import bookRouter from "./routes/bookRoute.js";
-import userRouter from "./routes/userRoute.js";
+import userRouter from "./routes/userRoute.js"; 
 import logRouter from "./routes/logRoute.js";
 
 dotenv.config();
@@ -14,33 +12,29 @@ await connectDB();
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const allowedOrigins = [
-  "https://mini-library-system.onrender.com",
+  'https://mini-library-system.onrender.com',
 ];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-  })
-);
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "DELETE", "PUT"]
+}));
 
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+
+app.use('/uploads', express.static('uploads')); 
 
 app.use("/api/books", bookRouter);
 app.use("/api/users", userRouter);
 app.use("/", logRouter);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+app.get("/", (req, res) => {
+  res.send(`
+        Go to main site: <a href="https://mini-library-manager.onrender.com" target="_blank">Hello World</a>
+  `);
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(8080, () => console.log("Server running on port 8080"));
